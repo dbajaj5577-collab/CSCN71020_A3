d17fd1e06174231e1613f77db0148b3edb5276d4
Daksh bajaj <dbajaj8991@conestoga.on.ca>
2026-03-12 19:03:16 -0400

    ENTERED C CODE FOR PLAYER 1 TO WIN

--- Game/game.c
+++ Game/game.c
@@ -19,10 +19,11 @@ const char* playGame(const char* player1Shape, const char* player2Shape)
         return STR_DRAW;
     }
     if ((strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_SISSOR) == 0) ||
-        (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0) ||
-        (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_PAPER) == 0)) {
+        (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_PAPER) == 0) ||
+        (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0)) {
         return STR_PLAYER1;
     }
+
 
     return STR_PLAYER2;
 }
