316e26436cd82a37580988f53399abde25b874ca
Daksh bajaj <dbajaj8991@conestoga.on.ca>
2026-03-12 17:51:21 -0400

    added code for invalid,Draw

--- Game/game.c
+++ Game/game.c
@@ -1,15 +1,27 @@
-// Game.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include "game.h"
+#include <string.h>
 
 const char* playGame(const char* player1Shape, const char* player2Shape)
 {
-	//#write functional code here
-	return STR_INVALID;
-}
+    if (strcmp(player1Shape, STR_ROCK) != 0
+        && strcmp(player1Shape, STR_PAPER) != 0
+        && strcmp(player1Shape, STR_SISSOR) != 0) {
+        return STR_INVALID;
 
-int main()
-{
-    return 1;
+    }
+    if (strcmp(player2Shape, STR_ROCK) != 0 
+        && strcmp(player2Shape, STR_PAPER) != 0 
+        &&  strcmp(player2Shape, STR_SISSOR) != 0) {
+        return STR_INVALID;
+    }
+
+    if (strcmp(player1Shape, player2Shape) == 0) {
+        return STR_DRAW;
+    }
+    if ((strcmp(player1Shape, STR_ROCK) == 0 && strcmp(player2Shape, STR_SISSOR) == 0) ||
+        (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0) ||
+        (strcmp(player1Shape, STR_SISSOR) == 0 && strcmp(player2Shape, STR_PAPER) == 0)) {
+        return STR_PLAYER1;
+    }
+    return STR_PLAYER2;
 }
--- UnitTestDriver/UnitTestDriver.cpp
+++ UnitTestDriver/UnitTestDriver.cpp
@@ -27,5 +27,33 @@ namespace UnitTestDriver
 		{
 			Assert::AreEqual(STR_INVALID, playGame("DOG","COW"));
 		}
+		TEST_METHOD(TestMethod5)
+		{
+			Assert::AreEqual(STR_PLAYER1, playGame(STR_PAPER, STR_ROCK));
+		}
+		TEST_METHOD(TestMethod6)
+		{
+			Assert::AreEqual(STR_PLAYER1, playGame(STR_ROCK, STR_SISSOR));
+		}
+		TEST_METHOD(TestMethod7)
+		{
+			Assert::AreEqual(STR_PLAYER1, playGame(STR_SISSOR, STR_PAPER));
+
+        //test cases of player 2 win
+		}
+		TEST_METHOD(TestMethod8)
+		{
+			Assert::AreEqual(STR_PLAYER2, playGame(STR_SISSOR, STR_ROCK));
+		}
+
+		TEST_METHOD(TestMethod9)
+		{
+			Assert::AreEqual(STR_PLAYER2, playGame(STR_ROCK, STR_PAPER));
+		}
+
+		TEST_METHOD(TestMethod10)
+		{
+			Assert::AreEqual(STR_PLAYER2, playGame(STR_PAPER, STR_SISSOR));
+		}
 	};
 }
