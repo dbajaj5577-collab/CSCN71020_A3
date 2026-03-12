713f70f4a5516e339915356429ddfd6cf77d66f6
Daksh bajaj <dbajaj8991@conestoga.on.ca>
2026-03-12 16:20:50 -0400

    ENTERED 4 TEST for "DRAW" and invalid test

--- UnitTestDriver/UnitTestDriver.cpp
+++ UnitTestDriver/UnitTestDriver.cpp
@@ -15,5 +15,17 @@ namespace UnitTestDriver
 		{
 			Assert::AreEqual(STR_DRAW, playGame(STR_ROCK, STR_ROCK));
 		}
+		TEST_METHOD(TestMethod2)
+		{
+			Assert::AreEqual(STR_DRAW, playGame(STR_SISSOR, STR_SISSOR));
+		}
+		TEST_METHOD(TestMethod3)
+		{
+			Assert::AreEqual(STR_DRAW, playGame(STR_PAPER, STR_PAPER));
+		}
+		TEST_METHOD(TestMethod4)
+		{
+			Assert::AreEqual(STR_INVALID, playGame("DOG","COW"));
+		}
 	};
 }
