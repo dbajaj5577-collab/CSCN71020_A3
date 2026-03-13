86aaed5ca688d76e4d02587d5d38fd41f1e6b87e
Daksh bajaj <dbajaj8991@conestoga.on.ca>
2026-03-12 21:34:42 -0400

    ADDED CODE FOR PLAYER 2 TO WIN

--- Game/game.c
+++ Game/game.c
@@ -23,5 +23,9 @@ const char* playGame(const char* player1Shape, const char* player2Shape)
         (strcmp(player1Shape, STR_PAPER) == 0 && strcmp(player2Shape, STR_ROCK) == 0)) {
         return STR_PLAYER1;
     }
-    
+    if ((strcmp(player2Shape, STR_ROCK) == 0 && strcmp(player1Shape, STR_SISSOR) == 0) ||
+        (strcmp(player2Shape, STR_SISSOR) == 0 && strcmp(player1Shape, STR_PAPER) == 0) ||
+        (strcmp(player2Shape, STR_PAPER) == 0 && strcmp(player1Shape, STR_ROCK) == 0)) {
+        return STR_PLAYER2;
+    }
 }
